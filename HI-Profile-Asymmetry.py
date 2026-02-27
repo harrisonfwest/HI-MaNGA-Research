cf829ba686724b48233ea5295619b360ac383fbf
HwestCode <hwest@haverford.edu>
2026-02-27 13:40:18 -0500

    Update HI-Profile-Asymmetry.py

--- HI-Profile-Asymmetry.py
+++ HI-Profile-Asymmetry.py
@@ -1,89 +1,25 @@
-# Python rewriting of profile_asymmetry.pro
+# Python script to calculate global HI profile asymmetry, loosely based on that in /HI Profile/profile_asymmetry.pro
 import numpy as np
 from numpy.typing import ArrayLike
 import matplotlib.pyplot as plt
 from astropy.io import fits
 
-# IDL has a robust_sigma() function taking in an array-like, which we will recreate:
-def robust_sigma(vals) -> float:
-    x = np.array(x)
-    median = np.median(x)
-    mad = np.median(np.abs(x - median))
-    return mad * 1.4826
+def profile_asymmetry(velocity: ArrayLike, flux: ArrayLike, VHI: float, VOPT: float, width: float | ArrayLike, plot: bool = False) -> ArrayLike:
+    """
+    Function to calculate asymmetry parameters of global gas profile
 
-def profile_asymmetry(velocity: ArrayLike, flux: ArrayLike, v0: float, vlow: float, vhigh: float, rms: float = None, plot:bool = False): # IDL function also had an 'inspect' keyword which halts the function before returning?
-    velocity = np.array(velocity)
-    flux = np.array(flux)
-    
-    if rms == None:
-        sel = np.sort(np.concatenate(np.argwhere(velocity < vlow), np.argwhere(velocity > vhigh)))
-        rms = robust_sigma(flux[sel])
-        
-    sel1 = np.sort(np.concatenate(np.argwhere(velocity >= vlow), np.argwhere(velocity <= v0)))
-    sel2 = np.sort(np.concatenate(np.argwhere(velocity >= v0), np.argwhere(velocity <= vhigh)))
-    count1 = len(sel1)
-    count2 = len(sel2)
-    
-    if count1 >= 2 and count2 >= 2:
-        flow = np.sum(flux[sel1])
-        eflow = rms * np.sqrt(count1)
-        fhigh = np.sum(flux[sel2])
-        efhigh = rms*np.sqrt(count2)
-        
-        ratio = flow/fhigh
-        eratio = np.sqrt((eflow/fhigh)**2 + (flow*efhigh/fhigh**2)**2)
-        
-        asym = np.log10(ratio)
-        easym = 1/np.log(10)*eratio/ratio
-    
-        if plot == True:
-            plt.plot(velocity, flux, c='black')
-            plt.axvline(v0, c = 'tab:green', lw = 3)
-            plt.plot(velocity[sel1], flux[sel1], c='tab:red')
-            plt.plot(velocity[sel2], flux[sel2], c='tab:blue')
-            plt.axhline(1.5*rms, linestyle = 2)
-            plt.show()
-    else:
-        asym = -999
-        easym = -999
-    
-    return [asym, easym]
+    Args:
+        velocity (ArrayLike): Array of velocities (i.e. x-axis of gas profile spectrum)
+        flux (ArrayLike): Array of fluxes (i.e. y-axis of gas profile spectrum)
+        VHI (float): Gas-derived central velocty of the galaxy
+        VOPT (float): Optically-derived central velocty of the galaxy
+        width (float | ArrayLike): Effective width of the galaxy along the velocity axis. Taken as the largest of the galaxy widths given in the MaNGA data table (i.e. WM50, WP50, etc.)
+        plot (bool, optional): Argument to determine if the chosen global gas profile will be plotted. Defaults to False.
 
-def asymmetry_himanga(id, asym, easym, v0 = None, path = None, plot = False):
-    # id should be SDSS ID
-    if path == None:
-        path = './'
-    par = path + id + '_par.sav' # TODO What is this file? IDL uses 'restore' which is not built into python the same way
-    spec = path + 'mangaHI-' + id + '.fits'
+    Returns:
+        ArrayLike: List of parameters passed into function call, as well as integrated fluxes for each of left (low velocity) and right (high velocity) sides of the galaxy for each of the provided central velocities. Asymmetry itself is not directly calculated to allow for flexibility in subsequent analysis
+    """
+    v0 = np.array([VHI, VOPT])
+    low_int_flux = -999 # Low velocity side integrated flux
+    hi_int_flux = -999 # High velocity side integrated flux
     
-    s = fits.open(spec)
-    hdr = s[0].header
-    if v0 == None:
-        v0 = hdr['OBJ_VEL']
-    rms = np.sqrt(np.mean(par**2)) # TODO this probably doesn't work, see earlier todo note
-    
-    if len(s[1]) >= 1000:
-        print('Error in spectrum size. Skipping')
-        return
-    
-    ### Remaining IDL in this procedure (not function, how to deal with?):
-    # awvinds = [par.awvinfo.xmin,par.awvinfo.xmax] - round(150/4)
-    # if awvinds[0] lt 0 or awvinds[1] lt 0 then return
-
-    # vmin = min([s.vhi[awvinds[0]],s.vhi[awvinds[1]]])
-    # vmax = max([s.vhi[awvinds[0]],s.vhi[awvinds[1]]])
-
-    # out = profile_asymmetry(s.vhi,s.fhi,v0,vmin,vmax,rms=rms,plot=1)
-    # asym = out[0]
-    # easym = out[1]
-
-def find_edges(velocity: ArrayLike, flux: ArrayLike, v0, rms = None, threshold = None, maxcount = None, reverse = False, plot = False, center_offset_start = None):
-    if maxcount == None:
-        maxcount = 3
-    if threshold == None:
-        threshold = 1.5
-    if center_offset_start == None:
-        center_offset_start = 50
-    
-    # Find index corresponding to v0
-    ind0 = np.argwhere(velocity == v0)
