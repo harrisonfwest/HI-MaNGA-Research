383b61d19dc5a358e6028c6ccfb7db3c5d427fc8
HwestCode <hwest@haverford.edu>
2026-03-18 15:16:11 -0400

    progress

--- HI_Profile_Asymmetry.py
+++ HI_Profile_Asymmetry.py
@@ -5,7 +5,12 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from scipy.integrate import trapezoid
 
-def find_edges(velocity: ArrayLike, flux: ArrayLike, v_central: float, threshold: float = 3, max_bins: int = 12) -> ArrayLike:
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+def find_edges(velocity: ArrayLike, flux: ArrayLike, v_central: float, threshold: float = 5, max_bins: int = 12) -> ArrayLike:
     """
     Algorithm to find edges of 1D spectrum (i.e. flux over a range of velocities) by detecting where the spectrum is reduced to noise
 
@@ -14,7 +19,7 @@ def find_edges(velocity: ArrayLike, flux: ArrayLike, v_central: float, threshold
         flux (ArrayLike): Array of fluxes (i.e. y-axis of gas profile spectrum)
         v_central (float): Central velocity of the galaxy
         rms (float, optional): Optional parameter of pre-calculated RMS noise. If no value is provided, it will be calculated from the spectrum. Defaults to None.
-        threshold (float, optional): Sigma level below which spectrum is considered noisy (i.e. above how many multiples of the RMS is the data a detection?). Defaults to 3.
+        threshold (float, optional): Sigma level below which spectrum is considered noisy (i.e. above how many multiples of the RMS is the data a detection?). Defaults to 5.
         max_bins (int, optional): Number of consecutive points required to be within the noise threshold to define an edge. Defaults to 12.
 
     Returns:
@@ -27,17 +32,17 @@ def find_edges(velocity: ArrayLike, flux: ArrayLike, v_central: float, threshold
     rms_sel = np.transpose(np.argwhere(abs(velocity - v_central) >= 750))[0][20:-20] # Trim first and last 20 velocities of baseline to remove artifacts, and assume 750 km/s away from center is baseline noise
     rms = np.sqrt(np.mean(flux[rms_sel]**2))
         
-    cen_vel = np.argwhere(velocity == v_central)
+    cen_vel = np.argwhere(velocity == find_nearest(velocity, v_central))[0, 0]
     low_vel = cen_vel - 200 # Assuming 200 indices is a reasonable range within which to find the edge of the galaxy
     high_vel = cen_vel + 200
     
     for i in range(cen_vel - max_bins + 1, low_vel):
-        if flux[i:i+max_bins - 1].all <= rms * threshold:
+        if flux[i:i+max_bins - 1].all() <= rms * threshold:
             low_edge = i+max_bins-1
             break
     
     for i in range(cen_vel + max_bins - 1, high_vel):
-        if flux[i:i-max_bins + 1].all <= rms * threshold:
+        if flux[i:i-max_bins + 1].all() <= rms * threshold:
             low_edge = i-max_bins+1
             break
     
@@ -49,30 +54,49 @@ def find_edges(velocity: ArrayLike, flux: ArrayLike, v_central: float, threshold
     
     return [low_edge, high_edge]
 
-
-def profile_asymmetry(plateIFU: str, velocity: ArrayLike, flux: ArrayLike, VHI: float, VOPT: float, widths: ArrayLike, plot: bool = False) -> ArrayLike:
-    """
-    Function to calculate asymmetry parameters of global gas profile
-
-    Args:
-        velocity (ArrayLike): Array of velocities (i.e. x-axis of gas profile spectrum)
-        flux (ArrayLike): Array of fluxes (i.e. y-axis of gas profile spectrum)
-        VHI (float): Gas-derived central velocty of the galaxy
-        VOPT (float): Optically-derived central velocty of the galaxy
-        width (ArrayLike): Effective widths of the galaxy along the velocity axis. Include WM50, WP50, WP20, W2P50, and WF50
-        plot (bool, optional): Argument to determine if the chosen global gas profile will be plotted. Defaults to False.
-
-    Returns:
-        ArrayLike: List of parameters passed into function call, as well as integrated fluxes for each of left (low velocity) and right (high velocity) sides of the galaxy
-        for each of the provided central velocities. Asymmetry itself is not directly calculated to allow for flexibility in subsequent analysis
-    """
+def spectrum_analysis(plateIFU: str, velocity: ArrayLike, flux: ArrayLike, VHI: float, VOPT: float, widths = ArrayLike, plot: bool = False) -> ArrayLike:
+    v0_arr = [VHI, VOPT]
+    flux_pairs = []
     
-    # instead of float parameter width, use ArrayLike widths corresponding to (in order) WM50, WP50, WP20, W2P50, and WF50
+    velocity_index_pairs = []
+    for v0 in v0_arr:
+        for w in widths:
+            lo_vel_index = np.argwhere(velocity == find_nearest(velocity, round(v0 - w)))[0, 0]
+            hi_vel_index = np.argwhere(velocity == find_nearest(velocity, round(v0 + w)))[0, 0]
+            arr = [lo_vel_index, hi_vel_index]
+            velocity_index_pairs.append(arr)
+        arr = find_edges(velocity, flux, v0)
+        velocity_index_pairs.append(arr)
     
-    color_count = 1 # For automatic coloring of same width-v0 paired lines (i.e. v0, v0-width, v0+width). See plotting block below
-    integrated_fluxes = np.empty([0]) # List of pairs of integrated fluxes for low and high velocity sides about central vel (not grouped within list)
-
-    if plot == True:
+    # velocity_index_pairs now ordered as follows:
+    # [VHI based WM50 indices], [VHI based WP50 indices], [VHI based WP20 indices], [VHI based W2P50 indices], [VHI based WF50 indices],
+    # [VOPT based WM50 indices], [VOPT based WP50 indices], [VOPT based WP20 indices], [VOPT based W2P50 indices], [VOPT based WF50 indices]
+    
+    velocity_index_pairs = velocity_index_pairs[1::]
+    print(velocity_index_pairs)
+    
+    for i in range(5):
+        lo_sel = np.transpose(np.argwhere((velocity > velocity_index_pairs[i][0]) & (velocity < VOPT)))[0]
+        hi_sel = np.transpose(np.argwhere((velocity < velocity_index_pairs[i][1]) & (velocity > VOPT)))[0]
+        lo_flux = trapezoid(flux[lo_sel], velocity[lo_sel])
+        hi_flux = trapezoid(flux[hi_sel], velocity[hi_sel])
+        flux_pairs = np.append(flux_pairs, [lo_flux, hi_flux])
+        
+    for i in range(5):
+        lo_sel = np.transpose(np.argwhere((velocity > velocity_index_pairs[i+5][0]) & (velocity < VOPT)))[0]
+        hi_sel = np.transpose(np.argwhere((velocity < velocity_index_pairs[i+5][1]) & (velocity > VOPT)))[0]
+        lo_flux = trapezoid(flux[lo_sel], velocity[lo_sel])
+        hi_flux = trapezoid(flux[hi_sel], velocity[hi_sel])
+        flux_pairs = np.append(flux_pairs, [lo_flux, hi_flux])
+    
+    # flux_pairs now contains left and right integrated fluxes for ordering described above
+    
+    res = np.array([plateIFU, v0_arr])
+    res.append(widths)
+    res = np.append(res, flux_pairs)
+    
+    if plot:
+        color_count = 1
         plt.figure(figsize= (10, 8))
         plt.plot(velocity, flux, color = 'black')
         plt.axhline(0, c='gray', lw = 2)
@@ -84,52 +108,16 @@ def profile_asymmetry(plateIFU: str, velocity: ArrayLike, flux: ArrayLike, VHI: 
         plt.axvline(VHI, lw = 2, color = 'lime', linestyle = '--', label = 'VHI central velocity')
         plt.axvline(VOPT, lw = 2, color = 'magenta', linestyle = '--', label = 'VOPT central velocity')
         
-    for width, widthname in zip(widths, ['WM50', 'WP50', 'WP20', 'W2P50', 'WF50']):    
-        # Calculate rms to subtract from flux
-        mean_v0 = (VHI + VOPT)/2
-        rms_sel = np.transpose(np.argwhere(abs(velocity - mean_v0) >= width))[0][20:-20] # Trim first and last 20 velocities of baseline to remove noise
-        rms = np.sqrt(np.mean(flux[rms_sel]**2))
-        subtracted_flux = flux - rms
-        
-        # low_edge_index, high_edge_index = find_edges(velocity, flux, mean_v0, rms)
-        
-        lo_sel_HI = np.transpose(np.argwhere((velocity > (VHI - width)) & (velocity < VHI)))[0]
-        hi_sel_HI = np.transpose(np.argwhere((velocity < (VHI + width)) & (velocity > VHI)))[0]
-
-        lo_sel_OPT = np.transpose(np.argwhere((velocity > (VOPT - width)) & (velocity < VOPT)))[0]    
-        hi_sel_OPT = np.transpose(np.argwhere((velocity < (VOPT + width)) & (velocity > VOPT)))[0]
-        
-        
-        # We will integrate using the Trapezoidal rule, implemented in scipy.integrate.trapezoid
-        
-        # Low velocity side integrated flux centered at VHI
-        lo_flux_HI = trapezoid(subtracted_flux[lo_sel_HI], velocity[lo_sel_HI])
-        integrated_fluxes = np.append(integrated_fluxes, lo_flux_HI)
-        # High velocity side integrated flux centered at VHI
-        hi_flux_HI = trapezoid(subtracted_flux[hi_sel_HI], velocity[hi_sel_HI])
-        integrated_fluxes = np.append(integrated_fluxes, hi_flux_HI)
-        
-        # Low velocity side integrated flux centered at VOPT
-        lo_flux_OPT = trapezoid(subtracted_flux[lo_sel_OPT], velocity[lo_sel_OPT])
-        integrated_fluxes = np.append(integrated_fluxes, lo_flux_OPT)
-        # High velocity side integrated flux centered at VOPT 
-        hi_flux_OPT = trapezoid(subtracted_flux[hi_sel_OPT], velocity[hi_sel_OPT])
-        integrated_fluxes = np.append(integrated_fluxes, hi_flux_OPT)
-        
-        
-        if plot == True: # Put this inside loop of widths
-            plt.axvline(VHI - width, lw = 2, color = 'C' + str(color_count), label = 'VHI-centered ' + widthname + ' width')
-            plt.axvline(VHI + width, lw = 2, color = 'C' + str(color_count))
+        for vels, width_name in velocity_index_pairs, np.append(widths, 'analytically calculated'):
+            if color_count < 6:
+                cen = 'VHI'
+            elif color_count >= 6:
+                cen = 'VOPT'
+            plt.axvline(vels[0], color = 'C' + str(color_count), label = cen + '-based' + width_name + 'edges')
+            plt.axvline(vels[1], color = 'C' + str(color_count))
             color_count += 1
-            
-            plt.axvline(VOPT - width, lw = 2, color = 'C' + str(color_count), label = 'VOPT-centered ' + widthname + ' width')
-            plt.axvline(VOPT + width, lw = 2, color = 'C' + str(color_count))
-            color_count += 1
-    
-    if plot == True: # Leave this outside loop of widths
+        
         plt.legend()
         plt.show()
-        
-    res = np.append(np.array([plateIFU, VHI, VOPT]), widths)
-    res = np.append(res, integrated_fluxes)
+    
     return res
